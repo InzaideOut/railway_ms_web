1aefe662ab1172bf0c6d15836948837b1ee924b8
Ahmad Khidir <khidirahmad05@gmail.com>
2024-05-28 19:27:02 +0100

    init

---
+++ components/buttons/buttons.jsx
@@ -0,0 +1,8 @@
+import styles from "buttons.module.css"
+
+
+export function PrimaryButton(params) {
+    return (
+        <button></button>
+    )
+}
---
+++ components/fields/fields.jsx
@@ -0,0 +1,7 @@
+import styles from "fields.module.css"
+
+export function TextField(params) {
+    return (
+        <div></div>
+    )
+}
