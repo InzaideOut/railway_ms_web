0a9d26e7dc5faf10c4ccacdda8500239bc415c07
Ahmad Khidir <khidirahmad05@gmail.com>
2024-05-28 19:39:45 +0100

    init

--- components/buttons/buttons.jsx
+++ components/buttons/buttons.jsx
@@ -1,7 +1,9 @@
-import styles from "buttons.module.css"
+import styles from "./buttons.module.css"
 
 
 export function PrimaryButton(params) {
+    // TODO: Implement the primary button on the design board
+    // Hint: Use the styles.primaryButton class
     return (
         <button></button>
     )
--- components/fields/fields.jsx
+++ components/fields/fields.jsx
@@ -1,6 +1,8 @@
-import styles from "fields.module.css"
+import styles from "./fields.module.css"
 
 export function TextField(params) {
+    // TODO: Implement the text field on the design board
+    // Hint: Use the styles.textField class
     return (
         <div></div>
     )
